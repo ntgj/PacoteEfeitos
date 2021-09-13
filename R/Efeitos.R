8fad83372a40bfb8465d3c813550094ebbf4ee25
ntgj <90542454+ntgj@users.noreply.github.com>
2021-09-13 02:03:50 -0300

    Update Efeitos.R

--- R/Efeitos.R
+++ R/Efeitos.R
@@ -19,6 +19,7 @@ Porcentagem <-function(X,y,k,PC){
   Porcentagem <- c(porc[1:length(Efeito),1])
   df <- data.frame(Efeitos,Porcentagem)
   ggplot(df, aes(x=Efeitos, y=Porcentagem,)) +  geom_bar(stat = "identity",color="Black", fill="Pink") +  theme_classic() + ylab("%")
+  + scale_x_continuous(breaks = seq(1,length(Efeito), by = 1))
 }
 
 #'Cria grafico de probabilidade de efeito
